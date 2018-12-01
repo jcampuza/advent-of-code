7436cf9f6f4229c0784ca3fedb78f0cee75ea3bb
Joseph Campuzano <campuzanojoseph@gmail.com>
2018-12-01 00:11:24 -0600

    updating readme with links and changing filenames to match better

--- src/01/part1.js
+++ src/01/part1.js
@@ -2,25 +2,11 @@ const fs = require("fs");
 const { promisify } = require("util");
 const readFile = promisify(fs.readFile);
 
-function processFrequency(str) {
-  const sign = str[0];
-  const value = parseInt(str.slice(1));
-
-  if (sign === "-") {
-    return -1 * value;
-  }
-
-  return value;
-}
-
 (async function main() {
   const input = await readFile("./input.txt");
   const values = input.toString().split("\n");
 
-  const frequency = values.reduce(
-    (acc, value) => acc + processFrequency(value),
-    0
-  );
+  const frequency = values.reduce((acc, value) => acc + Number(value), 0);
 
   console.log("ending frequency", frequency);
 })();
--- src/01/part2.js
+++ src/01/part2.js
@@ -2,27 +2,16 @@ const fs = require("fs");
 const { promisify } = require("util");
 const readFile = promisify(fs.readFile);
 
-function processFrequency(str) {
-  const sign = str[0];
-  const value = parseInt(str.slice(1));
-
-  if (sign === "-") {
-    return -1 * value;
-  }
-
-  return value;
-}
-
 (async function main() {
   const input = await readFile("./input.txt", "utf-8");
   const values = input.toString().split("\n");
 
   let frequency = 0;
-  let set = new Set();
+  let set = new Set([frequency]);
   let i = 0;
 
   while (true) {
-    frequency += processFrequency(values[i]);
+    frequency += Number(values[i]);
 
     if (set.has(frequency)) {
       break;
